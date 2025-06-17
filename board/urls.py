d6f73f5f7a5434ef385a46a308fdcfb9409cdd7a
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-17 14:45:57 +0900

    .

--- board/urls.py
+++ board/urls.py
@@ -8,4 +8,7 @@ app_name = 'board'  # board 앱의 URL 네임스페이스 설정
 urlpatterns = [
     path('', views.index, name='question_list'),  # board 앱의 index 뷰를 기본 URL로 설정
     path('<int:question_id>/', views.detail, name='question_detail'),  # 질문 상세 페이지 URL
+    path('ask/create/<int:question_id>/',
+         views.create_answer, name='create_answer'),  # 답변 생성 URL 
+    
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question, Answer
 
@@ -9,3 +9,9 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, id=question_id)  # 주어진 ID에 해당하는 질문이 없으면 404 에러 발생
     return render(request, 'board/detail.html', {'question': question})
+
+def create_answer(request, question_id):
+    question = get_object_or_404(Question, id=question_id)  # 주어진 ID에 해당하는 질문이 없으면 404 에러 발생
+    Answer(question=question,
+           content =request.POST.get('content')).save()  # POST 요청에서 'content' 필드의 값을 가져와 답변 생성
+    return redirect('board:question_detail', question_id=question.id)  # 질문 상세 페이지로 리다이렉트
