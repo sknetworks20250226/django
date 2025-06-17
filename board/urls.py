c1f21af8a21736d98bda677399340630fc73cbff
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-17 12:29:13 +0900

    .

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+
+# 네임스페이스
+# 사용할때는 board:question_detail 와 같이 사용
+app_name = 'board'  # board 앱의 URL 네임스페이스 설정
+
 urlpatterns = [
-    path('', views.index),  # board 앱의 index 뷰를 기본 URL로 설정
-    path('<int:question_id>/', views.detail),  # 질문 상세 페이지 URL
+    path('', views.index, name='question_list'),  # board 앱의 index 뷰를 기본 URL로 설정
+    path('<int:question_id>/', views.detail, name='question_detail'),  # 질문 상세 페이지 URL
 ]
