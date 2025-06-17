480641cbe77b8220ebb9c48be06becc89452227b
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-17 11:50:52 +0900

    .

--- board/urls.py
+++ board/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),  # board 앱의 index 뷰를 기본 URL로 설정
-    path('test/abc/', views.index),  # board 앱의 index 뷰를 기본 URL로 설정
+    path('<int:question_id>/', views.detail),  # 질문 상세 페이지 URL
 ]
--- board/views.py
+++ board/views.py
@@ -5,3 +5,7 @@ from .models import Question, Answer
 def index(request):
     questions = Question.objects.order_by('-created_at')  # 질문을 생성 시간 기준으로 내림차순 정렬
     return render(request, 'board/index.html', {'questions': questions})
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)    
+    return render(request, 'board/detail.html', {'question': question})
