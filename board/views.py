b43973761abb1b7df08497eb2657195d3a748824
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-17 12:14:46 +0900

    .

--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question, Answer
 
@@ -7,5 +7,5 @@ def index(request):
     return render(request, 'board/index.html', {'questions': questions})
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)    
+    question = get_object_or_404(Question, id=question_id)  # 주어진 ID에 해당하는 질문이 없으면 404 에러 발생
     return render(request, 'board/detail.html', {'question': question})
