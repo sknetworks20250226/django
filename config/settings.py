8b45346dcf1725119178ba93909161e77dbf52fb
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-17 10:49:38 +0900

    .

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'board.apps.BoardConfig',  # board 앱을 INSTALLED_APPS에 추가
 ]
 
 MIDDLEWARE = [
